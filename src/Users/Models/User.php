f8927bd9f015a4ab3317ae04571afa8a0b78ddbe
Matheus Gomes <matheus@hego.com.br>
2018-03-25 23:19:27 -0300

    Adicionando pacote tymon/jwt-auth e implementando o User model

--- src/Users/Models/User.php
+++ src/Users/Models/User.php
@@ -5,8 +5,9 @@ namespace HEGO\Domains\Users\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable,
         SoftDeletes;
@@ -33,4 +34,24 @@ class User extends Authenticatable
      * @var array
      */
     protected $dates = ['deleted_at'];
+    
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
